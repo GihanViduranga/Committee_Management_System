e1face7cda1ef46f56188e9e73ed3e73363d37a5
gihanviduranga <gihanvidu123@gmai.com>
2025-04-12 10:18:30 +0530

    Event participation Update

---
+++ src/main/java/lk/ijse/meethive/entity/Event_Participation.java
@@ -0,0 +1,29 @@
+package lk.ijse.meethive.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Event_Participation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int participation_id;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
